4c9ecc2485945f1e0648eb4fb74d7a9e9c073d86
PaulAhuevo <nieblerpaul@gmail.com>
2017-06-06 18:58:44 +0200

    implemented class Vec2D

--- src/Vec2D.java
+++ src/Vec2D.java
@@ -1,2 +1,21 @@
 public class Vec2D {
+
+    private double x;
+    private double y;
+
+    public void Vec2D(double x, double y) {
+
+
+
+    };
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+
+
+
+
 }

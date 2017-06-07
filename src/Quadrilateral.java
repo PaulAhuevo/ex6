96e89a4399567f561fefe924f311347f09dae258
PaulAhuevo <nieblerpaul@gmail.com>
2017-06-07 20:17:06 +0200

    implemented Quadrilateral

--- src/Quadrilateral.java
+++ src/Quadrilateral.java
@@ -1,2 +1,34 @@
-public class Quadriliteral {
+abstract public class Quadrilateral {
+
+    protected Vec2D pA;
+    protected Vec2D pB;
+    protected Vec2D pC;
+    protected Vec2D pD;
+
+    public Quadrilateral(Vec2D pA, Vec2D pB, Vec2D pC, Vec2D pD) {
+    }
+
+    abstract public double area();
+    abstract public double perimeter();
+
+    public static double area(Quadrilateral[] vs) {
+        return 0;
+    }
+
+    public static Trapezium[] parFilter(Quadrilateral[] vs) {
+        return new Trapezium[] {new Trapezium()};
+    }
+
+    public Vec2D getA() {return pA;}
+    public Vec2D getB() {return pB;}
+    public Vec2D getC() {return pC;}
+    public Vec2D getD() {return pD;}
+
+    public double getAB() {return Math.sqrt(((pA.getX()-pB.getX())*(pA.getX()-pB.getX())) + ((pA.getY()-pB.getY())*(pA.getY()-pB.getY())));}
+    public double getBC() {return Math.sqrt(((pB.getX()-pC.getX())*(pB.getX()-pC.getX())) + ((pB.getY()-pC.getY())*(pB.getY()-pC.getY())));}
+    public double getCD() {return Math.sqrt(((pC.getX()-pD.getX())*(pC.getX()-pD.getX())) + ((pC.getY()-pD.getY())*(pC.getY()-pD.getY())));}
+    public double getDA() {return Math.sqrt(((pD.getX()-pA.getX())*(pD.getX()-pA.getX())) + ((pD.getY()-pA.getY())*(pD.getY()-pA.getY())));}
+
+
+
 }
--- src/Vec2D.java
+++ src/Vec2D.java
@@ -3,11 +3,11 @@ public class Vec2D {
     private double x;
     private double y;
 
-    public void Vec2D(double x, double y) {
-
-
-
+    public Vec2D(double x, double y) {
+        this.x = x;
+        this.y = y;
     };
+
     public double getX() {
         return x;
     }
